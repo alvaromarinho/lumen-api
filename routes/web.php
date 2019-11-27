abde070cf7854f2dad298d851301a5ab337e1e5d
Álvaro Marinho <alvaro.marinho@live.com>
2019-11-27 07:52:42 -0300

    CORS midware

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,13 @@
 */
 
 /** @var \Laravel\Lumen\Routing\Router $router */
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
 $router->group(['prefix' => '/api', 'middleware' => 'auth'], function () use ($router) {
     
-    $router->get('/', function(){ return json_encode('Hello World!'); });
-
     $router->group(['prefix' => '/users'], function () use ($router) {
         $router->get('/', 'UsersController@index');
         $router->post('/', 'UsersController@store');
